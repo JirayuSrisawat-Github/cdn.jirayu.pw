c0b8183379670e3be822a9cadcbe0e0535695a45
JirayuSrisawat-Github <contact@jirayu.pw>
2023-06-01 17:28:30 +0700

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
-git add .
-git commit -m "Update"
+yarn build && \
+touch docs/.nojekyll && \
+echo "cdn.jirayu.pw" > docs/CNAME && \
+git add . && \
+git commit -m "Update" && \
 git push -u origin main
